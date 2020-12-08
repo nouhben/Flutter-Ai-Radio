4b118851bfcc2d484f545f22bc08bc3c32f5c643
nouh ben kadi <benkadi.nouh@icloud.com>
2020-12-08 01:44:46 +0100

    title

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:radio_ai/shared/constants.dart';
+import 'package:radio_ai/shared/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedContainer(
+      width: double.infinity,
+      height: SizeConfig.screenHeight,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            kAiPrimaryColor,
+            kAiSecondaryColor,
+          ],
+        ),
+      ),
+      duration: Duration(milliseconds: 200),
+    );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:radio_ai/providers/my_theme_provider.dart';
+import 'package:radio_ai/shared/size_config.dart';
 
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Ai Radio',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+      drawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: [
+              FlatButton.icon(
+                onPressed: () {
+                  Provider.of<MyThemeModel>(context).changeThem();
+                },
+                icon: Icon(Icons.settings),
+                label: Text('settings'),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Body(),
     );
   }
